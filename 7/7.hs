ff3568ac5ed78140396ac3b33bdd45442f5be7a6
Erik <erik.hermansson@gmail.com>
2020-12-08 13:05:16 +0100

    day 8

---
+++ 7/7.hs
@@ -0,0 +1,61 @@
+import Data.List
+import Data.Maybe (fromJust)
+import Data.List.Split
+import Data.Char
+import Data.Function
+
+-- | Node, made of String label and list of (Integer,Node) pairs
+data Node = Node
+    { label    :: String
+    , adjacent :: [(Int, Node)]
+    }
+    deriving (Show,Eq)
+
+-- | Graph, made of list of nodes
+data Graph = Graph [Node]
+    deriving (Show)
+
+-- | mkGraph creates a graph representaion of provided string
+mkGraph :: [(String, [String])] -> Graph
+mkGraph links = Graph $ map snd nodeLookupList where
+    
+    mkNode (lbl, adjacent) = (lbl, Node lbl $ map (\(x:y) -> (digitToInt x, lookupNode y)) adjacent)
+    
+    nodeLookupList = map mkNode links
+    
+    lookupNode lbl = fromJust $ lookup lbl nodeLookupList
+
+-- | findNode, searches Graph for a Node with label matching String
+findNode :: Graph -> String -> Maybe Node
+findNode (Graph []) _ = Nothing 
+findNode (Graph (Node n s:ns)) l |    n == l = Just (Node n s)
+                                 | otherwise = findNode (Graph ns) l
+
+-- | bagCount, given a bags adjecency list, counts the number of bags that can be contained in that bag
+bagCount :: [(Int, Node)] -> Int
+bagCount [] = 1
+bagCount ((i,n):xs) = i * (bagCount $ adjacent n) + (bagCount xs)
+
+-- | fitsIn, given a bags adjecency list, returns all unique bags that bag fits in
+fitsIn :: [(Int, Node)] -> [String]
+fitsIn [] = []
+fitsIn ((i,x):xs) = nub ([label x] ++ (fitsIn $ adjacent x) ++ (fitsIn xs))
+
+
+main = do
+      contents <- readFile "input.txt"
+      --read contents, split into lines
+      let bags = lines $ contents
+      --some dirty parsing to get a workable format. [parent, children..]
+      let l = [init $ splitOn "," $ concat [if isInfixOf "bag" z then  "," else z | z <- words bag, z /= "contain"] | bag <- bags]
+      --parent -> child map. each child prepended with related Int. [parent, [children]]
+      let cMap = [(head q, if isDigit (head $ head $ tail q) then map (\x -> x) (tail q) else [])| q <- l]
+      --child -> parent map. each parent prepended with dummy Int(since not used). [child, [parent]]
+      let pMap = map (\x -> (fst $ head x, foldl (\acc y -> acc ++ snd y) [] x)) $ groupBy ((==) `on` fst) $ sort $ concatMap (\(x,y) -> (x,[]) : map (\z -> (tail z,["0"++x])) y) cMap
+      --graphs for each task
+      let g1 = mkGraph pMap
+      let g2 = mkGraph cMap
+      --first task, how many bags does the shiny gold bag fit into? Solved by using child to parent graph and dfs
+      --second task, how many bags fit into the shiny gold bag?  Solved by using parent to child graph and dfs
+      return (length $ fitsIn $ adjacent $ fromJust $ findNode g1 "shinygold", bagCount (adjacent $ fromJust $ findNode g2 "shinygold") - 1)
+      
---
+++ 8/8.hs
@@ -0,0 +1,52 @@
+import Data.Char
+import Data.Maybe
+-- | stringToInt, converts string to integer
+stringToInt :: [Char] -> Int
+stringToInt = foldl addDigit 0
+   where addDigit num d = 10*num + digitToInt d
+-- | signedStringToInt, converts string prepended with sign to integer
+signedStringToInt :: [Char] -> Int
+signedStringToInt (c:cs) | c == '-'  = -1 * (stringToInt cs)
+                         | otherwise = stringToInt cs
+
+-- | noDuplicate, checks if instructions will lead to duplicate pointer location
+noDuplicate :: [(String,Int)] -> Int -> [Int] -> Bool
+noDuplicate instrs ptr prev | ptr >= (length instrs) = True
+                            | ptr `elem` prev        = False
+                            | instr == "nop"         = noDuplicate instrs (ptr+1) (ptr:prev)
+                            | instr == "acc"         = noDuplicate instrs (ptr+1) (ptr:prev)
+                            | instr == "jmp"         = noDuplicate instrs (ptr+num) (ptr:prev)
+                            where (instr,num) = instrs !! ptr
+
+-- | insertInstruction, inserts instruction v, at index i, in list l
+insertInstruction :: [(String,Int)] -> Int -> (String,Int) -> [(String,Int)]
+insertInstruction l i v = s ++ [v] ++ (tail e)
+                where (s,e) = splitAt i l
+
+-- | checkInstr2, follows instructions and corrects for the error, returning accumulated value when end is reached
+checkInstrs2 :: [(String,Int)] -> Int -> Int -> [Int] -> Bool -> Int
+checkInstrs2 instrs ptr acc prev alt | ptr >= (length instrs) = acc
+                                     | instr == "nop" && alt && altdups = checkInstrs2 altInstrs (ptr+num) acc (ptr:prev) False
+                                     | instr == "nop"  = (checkInstrs2 instrs (ptr+1) acc (ptr:prev) alt)
+                                     | instr == "jmp" && alt && altdups = checkInstrs2 altInstrs (ptr+1) acc (ptr:prev) False
+                                     | instr == "jmp"  = (checkInstrs2 instrs (ptr+num) acc (ptr:prev) alt)
+                                     | instr == "acc"  = checkInstrs2 instrs (ptr+1) (acc+num) (ptr:prev) alt                                     
+                                     where (instr,num)  = instrs !! ptr
+                                           altInstrs = if instr == "nop" then insertInstruction instrs ptr ("jmp",num) else insertInstruction instrs ptr ("nop",num)
+                                           altdups   = if instr == "nop" then (noDuplicate altInstrs (ptr+num) (ptr:prev)) else (noDuplicate altInstrs (ptr+1) (ptr:prev))
+
+-- | checkInstr, follows instructions and returns accumulated when a duplicate pointer position is reached
+checkInstrs :: [(String,Int)] -> Int -> Int -> [Int] -> Int
+checkInstrs instrs ptr acc prev | ptr >= (length instrs) || ptr `elem` prev = acc
+                                | instr == "nop"                           = checkInstrs instrs (ptr+1) acc (ptr:prev)
+                                | instr == "acc"                           = checkInstrs instrs (ptr+1) (acc+num) (ptr:prev)
+                                | instr == "jmp"                           = checkInstrs instrs (ptr+num) acc (ptr:prev)
+                                where (instr,num) = instrs !! ptr
+
+main = do
+      contents <- readFile "input.txt"
+      --read contents, split into lines
+      let instructions = [(ins,num) |line <- lines $ contents, let ins = (words line) !! 0, let num = signedStringToInt $ (words line) !! 1]
+      return (checkInstrs instructions 0 0 [], checkInstrs2 instructions 0 0 [] True)
+
+      
